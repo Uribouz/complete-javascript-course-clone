6b219276900451bdfad4d7d36ea1d6e15bb26a90
K0966149 <vatcharit.o@kbtg.tech>
2024-02-10 20:35:47 +0700

    WIP chapter 247 (min 10.07).

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,14 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+//chapter 247:
+
+//Examples
+
+const img = document.querySelector('.dog'); //Synchronous #1
+img.src = 'dog.jpg' //Asynchronous (#4)
+img.addEventListener('load', function() { //Synchronous #2
+    img.classList.add('fadeIn'); //Non-Asycnchronous, Just wait for the event to happen
+});
+p.style.width = '300px'; //Synchronous #3
